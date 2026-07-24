3f12ae99fbe171733547d9e86d3bfed75dc9ec6d
Himanchal <user.himanchal@gmail.com>
2026-07-24 17:32:06 +0545

    chore: establish design systems and project structure

---
+++ src/data/themes.ts
@@ -0,0 +1,19 @@
+import { ThemeOption } from "@/types";
+
+export const themes: ThemeOption[] = [
+  {
+    id: "dinosaur",
+    title: "Dinosaurs",
+    image: "/images/icons/dinosaur.png",
+  },
+  {
+    id: "vehicle",
+    title: "Vehicles",
+    image: "/images/icons/vehicle.png",
+  },
+  {
+    id: "animal",
+    title: "Animals",
+    image: "/images/icons/animal.png",
+  },
+];
---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = "Hamro Saathi";
+
+export const APP_TAGLINE = "A Gentle Learning Companion";
+
+export const LESSON_ORDER = ["matching", "counting", "sequencing"];
